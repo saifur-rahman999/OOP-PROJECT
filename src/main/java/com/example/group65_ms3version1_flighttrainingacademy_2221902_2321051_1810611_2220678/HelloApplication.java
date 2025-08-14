a1bc71c76b46b78d9e6771a16e2d1aee465145bd
Bhair-v <2220678@iub.edu.bd>
2025-08-14 21:26:10 +0600

    s

--- src/main/java/com/example/group65_ms3version1_flighttrainingacademy_2221902_2321051_1810611_2220678/HelloApplication.java
+++ src/main/java/com/example/group65_ms3version1_flighttrainingacademy_2221902_2321051_1810611_2220678/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("com.example.group65_ms3version1_flighttrainingacademy_2221902_2321051_1810611_2220678.FinanceAndEnrollmentOfficer.FEO_Dashboard.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
